502a184d2ad94fa78b69eb1ff0033fa79beef9d1
Erycles Junio <31996587+eryclesjunio@users.noreply.github.com>
2018-10-05 01:07:01 -0300

    Update wsgi.py

--- yoda/wsgi.py
+++ yoda/wsgi.py
@@ -1,11 +1,11 @@
-"""
+""""
 WSGI config for yoda project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
+""""
 
 import os
 
